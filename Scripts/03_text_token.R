34d3512450de865ed8f7b472a81b88a619d64211
camila-rahal <camila.batista@stud.unilu.ch>
2024-05-03 15:42:09 +0200

    Include new code for tokenization - stop words and wordcloud

--- Scripts/03_text_token.R
+++ Scripts/03_text_token.R
@@ -5,13 +5,70 @@
 # Date 03/05/2024
 # R version 4.3.1
 
-#------------Text tokenization
-words_df <- articles_df %>%
-  unnest_tokens(word, text)
 
-#--------removing stop words
-data(stop_words)
-words_df <- words_df %>%
-  #the anti_join function is used to remove all rows from word_df that has a word which is also present in stop_words
-  anti_join(stop_words, by = "word") 
+# Tokenization ------------------------------------------------------------
 
+library(tidyverse)
+library(tidytext)
+
+articles_df <- read_csv("articles_df.csv") %>% 
+  select(-article_id, -title, -body)
+
+tidy_script <- articles_df %>%
+  unnest_tokens(output = word, input = text)
+
+# Stop Words --------------------------------------------------------------
+
+View(stop_words)
+
+tidy_script <- tidy_script %>%
+  anti_join(stop_words, by = "word")
+
+plot_data <- tidy_script %>%
+  group_by(word, author) %>%
+  summarise(n = n()) %>% 
+  ungroup() %>% 
+  group_by(author) %>%
+  top_n(10) %>%
+  ungroup()
+
+plot_data %>% 
+  ggplot(aes(x = word, y = n, fill = author)) +
+  geom_col() +
+  facet_wrap(~author, ncol = 3, scales = "free_y") +
+  coord_flip() +
+  theme(axis.title.y = element_blank(),
+        axis.title.x = element_blank(),
+        legend.position = "none")
+
+res <- tidy_script %>%
+  group_by(word, author) %>%
+  summarise(n = n()) %>% 
+  ungroup() %>% 
+  group_by(author) %>%
+  top_n(1) %>%
+  ungroup() %>% 
+  arrange(desc(n))
+
+View(res)
+
+# Wordcloud ---------------------------------------------------------------
+
+library(tidyverse)
+library(tidytext)
+library(wordcloud2)
+
+tidy_script <- articles_df %>% 
+  unnest_tokens(output = word, input = text) %>% 
+  anti_join(stop_words, by ="word")
+
+plot_data <- tidy_script %>% 
+  filter() %>% 
+  group_by(word) %>% 
+  summarise(n = n()) %>% 
+  ungroup() %>% 
+  mutate(word = factor(word),
+         freq = as.numeric(n)) %>% 
+  arrange(desc(freq))
+
+wordcloud2(data = plot_data, size = 1, color = 'random-dark')
