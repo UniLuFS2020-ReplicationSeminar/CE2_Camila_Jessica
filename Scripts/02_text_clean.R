476657fb398685accf647a08ac9efe3cc268a26c
camila-rahal <camila.batista@stud.unilu.ch>
2024-05-03 15:43:16 +0200

    Cleaning comments / include save file

--- Scripts/02_text_clean.R
+++ Scripts/02_text_clean.R
@@ -5,10 +5,12 @@
 # Date 03/05/2024
 # R version 4.3.1
 
-# -------------Text cleaning
-#gsub function in R is used for pattern matching and replacement. It stands for "global substitution" 
-articles_df <- articles_df %>%
-  mutate(text = tolower(text),  #make all words lowercase
-         text = gsub("[[:punct:]]", "", text), #remove punctuation
-         text = gsub("[[:digit:]]", "", text))  #remove numbers
+# Text cleaning--------------------------------------------------------------
 
+articles_df <- articles_df %>%
+  mutate(text = tolower(text),  
+         text = gsub("[[:punct:]]", "", text), 
+         text = gsub("[[:digit:]]", "", text))  
+
+# Save data CSV file---------------------------------------------------------
+write.csv(articles_df, "articles_df.csv", row.names = FALSE)
